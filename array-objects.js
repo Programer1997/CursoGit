baf99cafec0e65ceecfee2df972c8d0f39647b62
abraham velazquez <wero210697@gmail.com>
2023-01-24 22:29:04 -0500

    probando filter,map and foreach con arrays

--- array-objects.js
+++ array-objects.js
@@ -1,16 +1,63 @@
-let arr = [true,true,false,true,false,true,true];
-
-arr.forEach(
+let arr = ["martin","angulo","maria","durando"];
+let arr2 =[true,true,true,false,false,true];
+/////////////////////////////////////////////////////
+/*arr.forEach(
     
     function (item,index){
 
     console.log(item);
 }
 
+);*/
+////////////////////////////////////////////////////
+/*let capitalizedWord=" ";
+let newArray=[];
+newArray= arr.map(
+
+    function (item,index){
+
+capitalizedWord = item[0].toUpperCase() + item.slice(1);
+
+return capitalizedWord 
+
+    }
 );
 
-/*arr.map(function (element,index){
+console.log(newArray);*/
 
-} );*/
+//////////////////////////////////////////////////
+/*let countT=0;
+arr.forEach(
+    
+function (state,possition){
 
+if (state == true){
+countT++;
+}
+return countT
 
+}
+
+);
+
+console.log(countT);*/
+//////////////////////////////////////////////
+
+/*FILTER METHOD
+let counter =0;
+arr2.filter(
+
+    function (item,index){
+
+        if (item == true){
+counter ++;
+return true
+        }
+        else {
+            return false
+        }
+
+    }
+);
+
+console.log(counter);*/
